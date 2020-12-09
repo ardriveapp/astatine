de382cddd923367d95534e11f686ff9f08559bbd
Philip Mataras <vilenarios@gmail.com>
2020-12-09 16:44:04 -0500

    Ensure data.size is treated as number

--- config.ts
+++ config.ts
@@ -102,21 +102,21 @@ async function get_24_hour_ardrive_transactions() : Promise<AstatineItem[]> {
         // We only want results from last 24 hours, defined by milliseconds since epoch
         if (yesterday.getTime() <= timeStamp.getTime()) {
           // We only want data transactions
-          if (data.size > 0) {
+          if (+data.size > 0) {
             // Does this wallet address exist in our array?
             let objIndex = weightedList.findIndex((obj => obj.address === owner.address));
             if (objIndex >= 0) {
             // If it exists, then we increment the existing data amount
               // console.log ("Existing wallet found %s with %s data", weightedList[objIndex].address, weightedList[objIndex].weight);
               // console.log("Adding ", data.size);
-              weightedList[objIndex].weight += data.size;
+              weightedList[objIndex].weight += +data.size;
             } 
             else {
               // Else we add a new user into our Astatine List
               // console.log("Adding new wallet ", owner.address);
               let arDriveUser: AstatineItem = {
                 address: owner.address,
-                weight: data.size,
+                weight: +data.size,
               };
               weightedList.push(arDriveUser);
             }
